5b965850c635486eca35f8e7772466c402af4f9c
Joshua <499747554@qq.com>
2020-11-01 11:21:23 +0800

    meta data config

--- bean-lifecycle/src/main/java/org/geekbang/thinking/in/spring/bean/lifecycle/BeanMetadataConfigurationDemo.java
+++ bean-lifecycle/src/main/java/org/geekbang/thinking/in/spring/bean/lifecycle/BeanMetadataConfigurationDemo.java
@@ -1,7 +1,11 @@
 package org.geekbang.thinking.in.spring.bean.lifecycle;
 
+import org.geekbang.thinking.in.spring.ioc.overview.domain.User;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.support.PropertiesBeanDefinitionReader;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.EncodedResource;
 
 /**
  * bean 元信息配置示例
@@ -13,7 +17,11 @@ public class BeanMetadataConfigurationDemo {
         DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
         PropertiesBeanDefinitionReader reader = new PropertiesBeanDefinitionReader(beanFactory);
         String location = "META-INF/user.properties";
-        int beanDefinitions = reader.loadBeanDefinitions(location);
+        Resource resource = new ClassPathResource(location);
+        EncodedResource encodedResource = new EncodedResource(resource, "GBK");
+        int beanDefinitions = reader.loadBeanDefinitions(encodedResource);
         System.out.println("读取的beanDefinition个数" + beanDefinitions);
+        User user = beanFactory.getBean("user", User.class);
+        System.out.println(user);
     }
 }
