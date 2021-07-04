5aa0ebfa572b608ce95fe93d063a51d2b3bc3104
Joshua <499747554@qq.com>
2021-07-04 17:39:42 +0800

    aop

---
+++ aop-features/src/main/java/org/geekbang/thinking/in/spring/aop/features/AspectJAnnotatedPointcutDemo.java
@@ -0,0 +1,33 @@
+package org.geekbang.thinking.in.spring.aop.features;
+
+/**
+ * Pointcut 示例
+ */
+
+import org.geekbang.thinking.in.spring.aop.features.aspect.AspectConfiguration;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration // Configuration class
+@EnableAspectJAutoProxy // 激活 Aspect 注解自动代理
+public class AspectJAnnotatedPointcutDemo {
+
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(AspectJAnnotatedPointcutDemo.class,
+                AspectConfiguration.class);
+        context.refresh();
+
+        AspectJAnnotatedPointcutDemo aspectJAnnotationDemo = context.getBean(AspectJAnnotatedPointcutDemo.class);
+
+        aspectJAnnotationDemo.execute();
+
+        context.close();
+    }
+
+    public void execute() {
+        System.out.println("execute()...");
+    }
+}
